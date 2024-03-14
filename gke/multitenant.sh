235da3f0da97e85af65024e68b6c3851e9b3faeb
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-03-14 16:26:28 +0100

    K8s secret files generation

--- gke/multitenant.sh
+++ gke/multitenant.sh
@@ -18,6 +18,8 @@ if [ ! -f $INST_ENV_FILE ]; then
 fi
 echo "Institution environment file found: $INST_ENV_FILE"
 source $INST_ENV_FILE
+echo "Institution environment variables loaded"
+echo "$SCANR_ES_HOST"
 
 if [ -d $INST_DIRECTORY ]; then
     read -p "Wipe instance directory $INST_DIRECTORY ? (y/n) " -n 1 -r
@@ -34,6 +36,32 @@ fi
 echo "Creating directory $INST_DIRECTORY for instance $INST"
 
 mkdir -p $INST_DIRECTORY
+
+YAML_DIR="svp"
+
+if [ ! -d "$YAML_DIR" ]; then
+    echo "Error: YAML_DIR '$YAML_DIR' does not exist."
+    exit 1
+fi
+
+base64_encode() {
+    echo -n "$1" | base64
+}
+
+for file in "$YAML_DIR"/*-secret.yaml; do
+
+    echo "Copying $file to $INST_DIRECTORY"
+    cp "$file" "$INST_DIRECTORY"
+
+    for var in SCANR_ES_HOST SCANR_ES_USER SCANR_ES_PASSWORD; do
+        value=$(eval "echo \$$var")
+        encoded_value=$(base64_encode "$value")
+        sed -i -e "s/\${$var}/$encoded_value/g" "$INST_DIRECTORY/$(basename "$file")"
+    done
+done
+
+
+# create namespace if it does not exist
 
 kubectl get namespace $INST > /dev/null 2>&1
 if [ $? -ne 0 ]; then
