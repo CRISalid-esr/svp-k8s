90f3140e11f0a93106344cb50d36b77707a1fa5e
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-10-09 15:51:45 +0200

    Add helm config and command

--- gke/multitenant-config-files.sh
+++ gke/multitenant-config-files.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 INST=$1
 
+CDB_TAG="0.2"
+
 if [ -z $INST ]; then
   echo "Please provide an institution name"
   exit 1
@@ -20,6 +22,7 @@ source $INST_ENV_FILE
 echo "Institution environment variables loaded"
 
 CONFIG_DIR="core/config"
+HELM_VALUES_DIR="core/helm"
 
 if [ ! -d "$CONFIG_DIR" ]; then
   echo "Error: CONFIG_DIR '$CONFIG_DIR' does not exist."
@@ -39,3 +42,16 @@ for file in "$CONFIG_DIR"/*-config.yaml; do
   done
 done
 
+# take all th * files from helm directory and copy them to inst directory
+# replace the variables in the files : LDAP_HOST LDAP_BIND_DN LDAP_BIND_PASSWORD AMQP_USER AMQP_PASSWORD
+for file in "$HELM_VALUES_DIR"/*-values.yaml; do
+
+  echo "Copying $file to $INST_DIRECTORY"
+  cp "$file" "$INST_DIRECTORY"
+
+  for var in LDAP_HOST LDAP_BIND_DN LDAP_BIND_PASSWORD AMQP_USER AMQP_PASSWORD CDB_TAG; do
+    value=$(eval "echo \$$var")
+    echo "Replacing $var with $value in $INST_DIRECTORY/$(basename "$file")"
+    sed -i -e "s#\${$var}#$value#g" "$INST_DIRECTORY/$(basename "$file")"
+  done
+done
