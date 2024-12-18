3f8c5ea62d97593519dc7b7dc5400d62d0c277b8
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-12-18 15:48:12 +0100

    Add missing env vars

--- gke/multitenant-secrets-files.sh
+++ gke/multitenant-secrets-files.sh
@@ -39,7 +39,9 @@ for file in "$SECRETS_DIR"/*-secret.yaml; do
     NEO4J_USER \
     NEO4J_PASSWORD\
     CTD_ES_USER \
-    CTD_ES_PASSWORD ; do
+    CTD_ES_PASSWORD \
+    NEXTAUTH_SECRET \
+    KEYCLOAK_CLIENT_SECRET; do
     value=$(eval "echo \$$var")
     encoded_value=$(base64_encode "$value")
     sed -i -e "s/\${$var}/$encoded_value/g" "$INST_DIRECTORY/$(basename "$file")"
