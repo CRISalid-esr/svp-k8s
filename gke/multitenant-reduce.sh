e1fba1b6ebea93056ade898aff57c16bda2e4ecf
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-01-07 13:34:32 +0100

    Downsize composer env

--- gke/multitenant-reduce.sh
+++ gke/multitenant-reduce.sh
@@ -12,3 +12,15 @@ kubectl scale --replicas=0 deployment svp-jel-proxy
 kubectl scale --replicas=0 deployment crisalid-training-data
 kubectl scale --replicas=0 deployment crisalid-bus
 kubectl scale --replicas=0 statefulset ctd-es-cluster-master
+
+gcloud composer environments describe $COMPOSER_ENV_NAME --location $LOCATION --format="value(name)" | grep -q $COMPOSER_ENV_NAME
+if [ $? -ne 0 ]; then
+  echo "Composer environment $COMPOSER_ENV_NAME does not exist"
+  exit 1
+else
+  echo "Composer environment $COMPOSER_ENV_NAME exists"
+fi
+
+gcloud composer environments update "$COMPOSER_ENV_NAME" \
+    --location="$LOCATION" \
+    --update-airflow-configs=scheduler-max_threads=0
