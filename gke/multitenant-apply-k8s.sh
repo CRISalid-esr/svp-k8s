4d9a9c1a802cb3d4c1939fd72287e3870d2a3413
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-06-04 15:15:28 +0200

    Upgrade IKG docker image tag

--- gke/multitenant-apply-k8s.sh
+++ gke/multitenant-apply-k8s.sh
@@ -5,7 +5,7 @@ source ./common.sh
 check_inst_arg
 load_inst_env
 
-SVPH_DOCKER_IMAGE_TAG="v0.19-dev"
+SVPH_DOCKER_IMAGE_TAG="v0.20-dev"
 SVPH_DOCKER_IMAGE_NAME="crisalidesr/svp-harvester"
 IKG_DOCKER_IMAGE_TAG="v0.17-dev"
 IKG_DOCKER_IMAGE_NAME="crisalidesr/crisalid-ikg"
