c7689a51eb24d5f0f0c92b64886f4bb70f06aac8
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-07-05 07:51:08 +0200

    Hotfix : adjust resources

--- gke/multitenant-apply-k8s.sh
+++ gke/multitenant-apply-k8s.sh
@@ -7,7 +7,7 @@ load_inst_env
 
 SVPH_DOCKER_IMAGE_TAG="v0.24-dev"
 SVPH_DOCKER_IMAGE_NAME="crisalidesr/svp-harvester"
-IKG_DOCKER_IMAGE_TAG="v0.19-dev"
+IKG_DOCKER_IMAGE_TAG="v0.21-dev"
 IKG_DOCKER_IMAGE_NAME="crisalidesr/crisalid-ikg"
 SVP_DOCKER_IMAGE_TAG="v0.38-dev"
 SVP_DOCKER_IMAGE_NAME="crisalidesr/sovisuplus"
